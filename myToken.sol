9f460d5607f517e6f523ecb6bd429b6324a1e2cc
Srijith <30682653+srijithsrk@users.noreply.github.com>
2018-10-09 01:49:21 +0530

    deployed and tested

--- myToken.sol
+++ myToken.sol
@@ -9,12 +9,16 @@ uint constant public decimal = 18;  //equivalent to wei
 uint constant public _tokenSupply = 1000;
 mapping(address => uint) balances;
 mapping(address => mapping(address => uint) allowed  //assigning delegates with some of our tokens
-  //event Transfer(address indexed from, address indexed to, uint tokens);
-  //event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+event Transfer(address indexed from, address indexed to, uint tokens);
+event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+function MyToken() public         //assigning tokens to the deployer of smart contract
+{
+balances[msg.sender] = _totalSupply;
 function totalSupply() public pure returns (uint)
 {
 return _tokenSupply;
 }
+
 function balanceOf(address tokenOwner) public constant returns (uint balance)
 {
 return balances[tokenOwner];        //gives the balance of the token address
@@ -28,7 +32,7 @@ return allowed[tokenOwner][spender];
 
 function transfer(address to, uint tokens) public returns (bool success)
 {
-require(tokens > 0 && balance[to] >= tokens);  // tokens can be transferred only when his existing balance has that ammount
+require(tokens > 0 && balance[msg.sender] >= tokens);  // tokens can be transferred only when his existing balance has that ammount
 // ignorance of spamming is also considered
 balances[msg.sender] = balances[msg.sender].sub(tokens);  //for deduction of amount of sender after transferring
 // sub used to prevent overflow
@@ -49,7 +53,7 @@ function transferFrom(address from, address to, uint tokens) public returns (boo
 {
 require(tokens > 0 && balances[from] >= tokens;
 balances[from] = balances[from].sub(tokens);
-allowed[msg.sender][from] = allowed[msg.sender][from].sub(tokens);
+allowed[from][msg.sender] = allowed[from][msg.sender].sub(tokens);
 // depreciating the value of the token creator who assigned delegates
 balances[to] = balances[to].add[tokens];
 Transfer(from, to, tokens);  //events
